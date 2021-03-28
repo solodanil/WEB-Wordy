2b3bf46da052f008ebccc914d51e39a5420723bb
solodanil <solo.danil@mail.ru>
2021-03-28 22:48:01 +0400

    Починил ссылки

--- main.py
+++ main.py
@@ -8,6 +8,10 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/clubs')
+def clubs():
+    return render_template('clubs.html')
+
 
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1')
