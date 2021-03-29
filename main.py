0256c50b91b524ea5025828d7b4da1563b4b6c74
drammma <Safonova14092004@yandex.ru>
2021-03-29 22:25:12 +0400

    продолжаю верстать + небольшие изменения

--- main.py
+++ main.py
@@ -12,6 +12,10 @@ def index():
 def clubs():
     return render_template('clubs.html')
 
+@app.route('/clubs/0')
+def club_page():
+    return render_template('club_page.html')
+
 
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1')
