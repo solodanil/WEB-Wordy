351b2c32923f1efee40d559021c3d07fa10217e7
solodanil <solo.danil@mail.ru>
2021-04-05 22:39:53 +0400

    Вот теперь норм коммит

--- main.py
+++ main.py
@@ -8,13 +8,20 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+
 @app.route('/clubs')
 def clubs():
     return render_template('clubs.html')
 
+
 @app.route('/clubs/0')
 def club_page():
     return render_template('club_page.html')
+
+
+@app.route('/word')
+def word():
+    return render_template('word.html')
 
 
 if __name__ == '__main__':
