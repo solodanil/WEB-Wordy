d6a6b0429676bed002ddc80a0d6d382583534016
solodanil <solo.danil@mail.ru>
2021-04-10 20:02:23 +0400

    Табличка для связи подборки и слова

--- data/collection.py
+++ data/collection.py
@@ -3,8 +3,17 @@ import sqlalchemy
 
 from .db_session import SqlAlchemyBase
 
+word_to_collection = sqlalchemy.Table(
+    'word_to_collection',
+    SqlAlchemyBase.metadata,
+    sqlalchemy.Column('collection', sqlalchemy.Integer,
+                      sqlalchemy.ForeignKey('collection.id')),
+    sqlalchemy.Column('word', sqlalchemy.String)
+)
+
+
 class Collection(SqlAlchemyBase):
-    __tablename__ = 'сollection'
+    __tablename__ = 'collection'
 
     id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True, autoincrement=True, index=True)
     name = sqlalchemy.Column(sqlalchemy.String, nullable=True)
