86141446643fa6f4da1566a6e836bae5e62b51b4
solodanil <solo.danil@mail.ru>
2021-04-15 14:16:46 +0400

    Переводчик и эмоджи (в emoji_translate нужно вместо импорта regrex сделать импорт re)

---
+++ dictionary.py
@@ -0,0 +1,24 @@
+import requests
+import json
+
+import translators as ts
+from emoji_translate.emoji_translate import Translator
+
+
+def google_dict(word, lang='en_US'):
+    response = requests.get(f'https://api.dictionaryapi.dev/api/v2/entries/{lang}/{word}').json()
+    print(response)
+    return response
+
+
+def translate(word, lang='ru'):
+    return ts.google(word, to_language=lang)
+
+
+def emoji(word):
+    emo = Translator(exact_match_only=False, randomize=True)
+    return emo.emojify(word)
+
+
+if __name__ == '__main__':
+    print(emoji('hello'))
