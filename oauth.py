0d7556e0ffb05d37afde60051beb15d655d53311
solodanil <solo.danil@mail.ru>
2021-04-18 23:29:57 +0400

    Я вроде начал oauth, но не закончил 2

--- oauth.py
+++ oauth.py
@@ -43,15 +43,42 @@ class VKSignIn(OAuthSignIn):
             client_secret=self.consumer_secret,
             authorize_url='https://oauth.vk.com/authorize',
             access_token_url='https://oauth.vk.com/access_token',
-            base_url='https://api.vk.com/method/'
+            base_url='https://api.vk.com/method/',
         )
 
     def authorize(self):
+        print(0)
+        print(self.service.get_authorize_url(
+            scope='friends',
+            response_type='code',
+            redirect_uri=self.get_callback_url(),
+            v='5.130'))
+        print(1)
         return redirect(self.service.get_authorize_url(
             scope='friends',
-            response_type='token',
+            response_type='code',
             redirect_uri=self.get_callback_url())
         )
 
     def callback(self):
-        pass
+        def decode_json(payload):
+            return json.loads(payload.decode('utf-8'))
+
+        if 'code' not in request.args:
+            return None, None, None
+        oauth_session = self.service.get_auth_session(
+            data={'code': request.args['code'],
+                  'v': '5.130',
+                  'grant_type': 'authorization_code',
+                  'redirect_uri': self.get_callback_url()},
+            decoder=decode_json
+        )
+        me = oauth_session.get('me').json()
+        print(me)
+        # return (
+        #     'facebook$' + me['id'],
+        #     me.get('email').split('@')[0],  # Facebook does not provide
+        #                                     # username, so the email's user
+        #                                     # is used instead
+        #     me.get('email')
+        # )
